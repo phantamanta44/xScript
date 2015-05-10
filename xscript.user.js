0133cb10436ab99150a5c297d7e6690aabbfb187
Phanta <evandalong@gmail.com>
2015-05-10 09:45:18 -0500

    Userscript

---
+++ xscript.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           xScript
+// @version        1.0
+// @description    Loads xScript via jQuery
+// @author         Phanta
+// @namespace      http://phantamanta44.github.io
+// @include        /^https?://(www\.)?plug.dj/.*/
+// @run-at         document-end
+// @grant          none
+// ==/UserScript==
+
+$.getScript('https://cdn.rawgit.com/phantamanta44/xScript/master/xscript.js');
